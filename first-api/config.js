e5fbfe56d906153e9941b1cf89b14659557215dd
Mike <mikeriberj@gmail.com>
2020-02-21 11:00:53 +0100

    added configuration

---
+++ first-api/config.js
@@ -0,0 +1,27 @@
+// Create and export configuration variables
+
+// Container for all the enviroments
+var enviroments = {};
+
+
+// Staging (default) enviroment
+enviroments.staging = {
+    'port' : 1337,
+    'envName' : 'staging'
+};
+
+// Production enviroment
+enviroments.production = {
+    'port' : 5000,
+    'envName' : 'production'
+};
+
+// Determines which enviroment was passed as a command-line argument
+var currentEnviroment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase : '';
+
+
+// Check that the current enviroment is one of the enviroments aboce, if not, default to staging
+var enviromentToExport = typeof(enviroments[currentEnviroment]) == 'object' ? enviroments[currentEnviroment] : enviroments.staging;
+
+// Export module
+module.exports = enviromentToExport;
--- first-api/index.js
+++ first-api/index.js
@@ -21,6 +21,7 @@
 const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
 
 // The server should respond to all requests with a string
 const server = http.createServer(function(req,res){
@@ -75,8 +76,8 @@ const server = http.createServer(function(req,res){
             var payloadString = JSON.stringify(payload);
 
             // Return the response
+            res.setHeader('Content-type','application/json');
             res.writeHead(statusCode);
-
             res.end(payloadString);
 
             // Log the request path
@@ -88,9 +89,9 @@ const server = http.createServer(function(req,res){
     });
 }); 
 
-// Start the server, and have it listen on port 1337
-server.listen(1337, function(){
-    console.log("Server is listening on port 1337 now");
+// Start the server
+server.listen(config.port, function(){
+    console.log("Server is listening on port: " + config.port + " in " + config.envName + " mode");
 });
 
 // Define handlers
