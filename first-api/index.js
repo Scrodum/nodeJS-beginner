551e691c9733e232bf2b24c50b8d0a53ace32788
Mike <mikeriberj@gmail.com>
2020-02-21 10:09:54 +0100

    adding routing requests

--- first-api/index.js
+++ first-api/index.js
@@ -50,18 +50,64 @@ const server = http.createServer(function(req,res){
     req.on('end', function() {
         buffer += decoder.end();
 
-        // Send the response
-        res.end("Hello World\n");
-        
-        // Log the request path
-        // console.log('Request received on path: ' + trimmedPath + ' With this method: ' + method + ' and with these querystring paramenters', queryStringObject);
-        // console.log("Request received with these headers: ", headers);
-        console.log("Request received with this payload: ", buffer);
-    });
+        // Choose the handler this request should go to
+        // If not found, go to not found handler
+        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
+        // Construct the data object to send to the handler
+        var data = {
+            'trimmedPath' : trimmedPath,
+            'queryStringObject' : queryStringObject,
+            'method' : method,
+            'headers' : headers,
+            'payload' : buffer
+        };
+
+        // Route the request to the handler specified in the router
+        chosenHandler(data, function(statusCode, payload) {
+            // Use the status code called back by the handler, or default to 200
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+            // Use the payload called back by the handler, or default to an empty object
+            payload = typeof(payload) == 'object' ? payload : {};
+
+            // Convert object to string
+            var payloadString = JSON.stringify(payload);
+
+            // Return the response
+            res.writeHead(statusCode);
+
+            res.end(payloadString);
+
+            // Log the request path
+            // console.log('Request received on path: ' + trimmedPath + ' With this method: ' + method + ' and with these querystring paramenters', queryStringObject);
+            // console.log("Request received with these headers: ", headers);
+            // console.log("Request received with this payload: ", buffer);
+            console.log('Returning the response: ', statusCode, payloadString);
+        });
+    });
 }); 
 
 // Start the server, and have it listen on port 1337
 server.listen(1337, function(){
     console.log("Server is listening on port 1337 now");
 });
+
+// Define handlers
+var handlers = {};
+
+// Sample handler
+handlers.sample = function(data, callback) {
+    // callback a http status code, and a payload object
+    callback(406,{'name' : 'sample handler'});
+};
+
+// Not found handler
+handlers.notFound = function(data, callback) {
+    callback(404);
+};
+
+// Define a request router
+var router = {
+    'sample' : handlers.sample
+};
