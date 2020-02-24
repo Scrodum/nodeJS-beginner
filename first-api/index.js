97532218f42b73c13f8c73245e55956768bdb0b8
Mike <mikeriberj@gmail.com>
2020-02-24 22:07:29 +0100

    added /ping with a callback of 200

--- first-api/index.js
+++ first-api/index.js
@@ -118,11 +118,10 @@ var unifiedServer = function(req, res) {
 // Define handlers
 var handlers = {};
 
-// Sample handler
-handlers.sample = function(data, callback) {
-    // callback a http status code, and a payload object
-    callback(406,{'name' : 'sample handler'});
-};
+// Ping handler
+handlers.ping = function(data, callback) {
+    callback(200);
+}
 
 // Not found handler
 handlers.notFound = function(data, callback) {
@@ -131,5 +130,5 @@ handlers.notFound = function(data, callback) {
 
 // Define a request router
 var router = {
-    'sample' : handlers.sample
+    'ping' : handlers.ping
 };
